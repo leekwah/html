976aa9ca58e6fabe6de47503156e2768b4a0c8e0
eeaoo <104570042+eeaoo@users.noreply.github.com>
2022-06-30 16:43:40 +0900

    20220630 16:43

---
+++ html/javascript/09.삼항조건연산자.js
@@ -0,0 +1,7 @@
+var a = 10;
+var b = 3;
+// 기본형
+// 조건식 ? 자바스크립트 코드1 : 자바스크립트 코드2;
+
+var result = a > b ? "JavaScript" : "Hello"; // True인지 False에 따라서 값이 달라진다.
+document.write(result); // True면 JavaScript가 출력되고, False면 Hello가 출력된다.
