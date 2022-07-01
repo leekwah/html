e37d0a8a304012fe67ce0962116af2cc07b56bbf
eeaoo <104570042+eeaoo@users.noreply.github.com>
2022-07-01 15:46:25 +0900

    20220701 15:46

--- html/javascript/10.체중계산기.js
+++ html/javascript/10.체중계산기.js
@@ -1,4 +1,4 @@
-var Name = prompt("당신의 이름은?", "");
+var yourName = prompt("당신의 이름은?", "");
 var height = prompt("당신의 신장은?", "0");
 var weight = prompt("당신의 몸무게는?", "00");
 
@@ -7,4 +7,4 @@ var normal_weight = (height - 100) * 0.9;
 var result = weight >= normal_weight -5 && weight <= normal_weight + 5;
 
 result = result ? "적정 체중입니다." : "적정 체중이 아닙니다.";
-document.write(Name + "님은" + result)
+document.write(yourName + "님은" + result)
