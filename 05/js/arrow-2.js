173b12cf4bffaf80f8697275c7968233ab2cbf7a
kwah <kwah@donga.ac.kr>
2022-07-04 22:34:54 +0900

    20220704 22:34

--- 05/js/arrow-2.js
+++ 05/js/arrow-2.js
@@ -6,4 +6,4 @@
         
 let hi = user => { document.write (user + "님, 안녕하세요?"); }
         
- hi("경희");
+ hi("크와");
--- 05/js/hoisting.js
+++ 05/js/hoisting.js
@@ -3,6 +3,6 @@ var x = 100;
 test();
 
 function test() { 			
-	document.write("x is " + x + ", y is " + y);
+  document.write("x is " + x + ", y is " + y);
   var y = 200;
 }
